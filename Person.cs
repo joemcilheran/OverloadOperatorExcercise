bdf7c6810e6ce471a8090d813726ad10c69eb625
joemcilheran <joe.mcilheran@gmail.com>
2016-05-10 11:08:37 -0500

    initial commit

---
+++ Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age
+{
+    class Person
+    {
+        public string firstName;
+        public string lastName;
+        public int age;
+        public int birthYear;
+        public Person(int BirthYear)
+        {
+            birthYear = BirthYear;
+            firstName = "bill";
+            lastName = "gill";
+            age = (2016 - birthYear);
+
+        }
+        public static Person operator +(Person person, int yearsToAge)
+        {          
+            Person agedPerson = new Person(person.birthYear);
+            agedPerson.age = (person.age + yearsToAge);
+            return agedPerson;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int birthYear = 1984;
+            Person person = new Person(birthYear);
+            Person agedPerson = person + 12;
+            Console.WriteLine("{0} {1} is {2} years old this year", person.firstName, person.lastName, person.age);
+            Console.WriteLine("{0} {1} will be {2} in 12 years", agedPerson.firstName, agedPerson.lastName, agedPerson.age);
+            Console.ReadKey();
+        }
+    }
+}
